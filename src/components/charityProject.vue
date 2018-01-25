9c306e54a1af77b4188ab4cf12fda2ff80536b4e
juli0gt <julio.gteran@gmail.com>
2018-01-25 22:50:23 +0000

    Change eth amount

--- src/components/charityProject.vue
+++ src/components/charityProject.vue
@@ -1,34 +1,90 @@
 <template lang="html">
-  <div class="project-container">
+    <div class="project-container">
 
-    <div class="image-container">
-        <img :src="projectData.url">
-    </div>
-    <div class="info-container">
-      <div class="project-text">
-        <p class="title uppercase bold">{{projectData.title}}</p>
-        <div class="row-aligned">
-          <label class="text-label uppercase" for="mission">{{text.mission}}</label>
-          <p class="text mission-height"> {{projectData.mission}}</p>
+        <div class="image-container">
+            <img :src="projectData.url">
         </div>
-        <div class="row-aligned">
-          <label class="text-label uppercase" for="approach">{{text.approach}}</label>
-          <p class="text approach-height">{{projectData.approach}}</p>
+
+        <div class="info-container">
+
+            <div class="project-text">
+
+                <p class="title uppercase bold">{{projectData.title}}</p>
+                <div class="row-aligned">
+
+                    <label class="text-label uppercase"
+                           for="mission">
+                        {{text.mission}}
+                    </label>
+
+                    <p class="text mission-height"> {{projectData.mission}}</p>
+                </div>
+
+                <div class="row-aligned">
+                    <label class="text-label uppercase" for="approach">{{text.approach}}</label>
+                    <p class="text approach-height">{{projectData.approach}}</p>
+                </div>
+            </div>
+
+          <div v-if="amountDonationOpen === false">
+
+
+              <p class="amount-text center-align">
+                  Help us, or visit our website for more information.
+              </p>
+
+              <div class="buttons-container confirm-buttons">
+
+                  <button v-if="metamaskAvailable"
+                          @click="openInsertAmount"
+                          type="button" name="button"
+                          class="uppercase button bold">
+                      {{text.button.donate}}
+                  </button>
+
+                  <a class="button uppercase bold" :href="projectData.website">{{text.website}}</a>
+
+              </div>
+
+          </div>
+
+          <div v-if="amountDonationOpen">
+
+              <div class="donation-amount">
+                   <p class="amount-text left-align">
+                      Insert the amount of ETH you want to donate:
+                   </p>
+
+                   <input class="input-line"
+                          type="amount"
+                          name="amount"
+                          placeholder="ETH"
+                          v-model="ethAmount">
+              </div>
+
+              <div class="buttons-container confirm-buttons">
+
+                  <button v-if="metamaskAvailable"
+                          @click="makeDonation"
+                          :disabled="disableDonate"
+                          type="button" name="button"
+                          class="uppercase button bold">
+                      {{text.button.confirm}}
+                  </button>
+
+                  <button v-if="metamaskAvailable"
+                          @click="cancelInsertAmount"
+                          type="button" name="button"
+                          class="uppercase button bold">
+                      {{text.button.cancel}}
+                  </button>
+
+              </div>
+
+          </div>
+
         </div>
-      </div>
-      <div class="donation-container">
-
-        <button v-if="metamaskAvailable"
-                @click="makeDonation"
-                type="button" name="button"
-                class="uppercase button bold">
-            {{text.button}}
-        </button>
-
-        <a class="button uppercase bold" :href="projectData.website">{{text.website}}</a>
-      </div>
     </div>
-  </div>
 
 </template>
 
@@ -40,15 +96,25 @@ export default {
     text: {
       mission: 'mission:',
       approach: 'approach:',
-      button: 'donate',
       website: 'visit website',
+      amount: 'amount',
+      button: {
+        donate: 'donate',
+        confirm: 'confirm',
+        cancel: 'cancel',
+      },
     },
+    amountDonationOpen: false,
+    ethAmount: 'ETH',
   }),
 
   computed: {
     metamaskAvailable() {
       if (typeof window.web3 !== 'undefined') return true;
       return false;
+    },
+    disableDonate() {
+      return !this.ethAmount || this.ethAmount === '0';
     },
   },
 
@@ -60,11 +126,19 @@ export default {
       const address = this.projectData.address;
       web3.eth.sendTransaction({
         to: address,
-        value: web3.toWei('1', 'ether'),
+        value: web3.toWei(this.ethAmount, 'ether'),
       }, (err) => {
         // eslint-disable-next-line
         console.log(err);
       });
+      this.cancelInsertAmount();
+    },
+    openInsertAmount() {
+      this.ethAmount = 0;
+      this.amountDonationOpen = true;
+    },
+    cancelInsertAmount() {
+      this.amountDonationOpen = false;
     },
   },
 };
@@ -94,7 +168,7 @@ img {
 
 .info-container {
   background-color: #FFFFFF;
-  padding: 20px 10px 20px 10px;
+  padding-top: 20px;
   border-radius: 0 0 10px 10px;
   border-top: 1px solid #F0F8FF;
 }
@@ -105,6 +179,9 @@ img {
 }
 
 .project-text {
+  padding: 0 10px;
+  border-bottom: 1px solid #F0F8FF;
+
   p {
     margin: 0;
   }
@@ -115,20 +192,20 @@ img {
     padding-bottom: 20px;
   }
 
-  .text-label {
-    width: 100px;
-    color: #63AA9C;
-    padding-right: 10px;
-    text-align: left;
-    font-weight: 300;
-  }
-
   p.text {
     width: 300px;
     margin: 0;
     text-align: left;
     color: #221E1D;
   }
+}
+
+.text-label {
+  width: 100px;
+  color: #63AA9C;
+  padding-right: 10px;
+  text-align: left;
+  font-weight: 300;
 }
 
 .mission-height {
@@ -138,12 +215,25 @@ img {
 .approach-height {
   height: 190px;
 }
-.donation-container {
+
+.donation-amount {
+  display: flex;
+}
+
+.donate-buttons {
+  height: 110px;
+}
+
+.confirm-buttons {
+  height: 66px;
+}
+
+.buttons-container {
   margin: 0 auto;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 80%;
-  padding-top: 20px;
   .button {
     border-radius: 17px;
     padding: 10px 0 10px 0;
@@ -153,10 +243,42 @@ img {
     outline: 0;
     font-size: 11px;
     width: 110px;
+    cursor: pointer;
+  }
+  button.button:disabled {
+      background-color: #D0D1E3;
+      color: #FFFFFF;
+      cursor: initial;
   }
   a {
     text-decoration: none;
   }
+}
+
+p.amount-text {
+  font-size: 14px;
+  padding: 0 10px;
+}
+
+.center-align {
+  text-align: center;
+  color: #de9494;
+}
+
+.left-align {
+  text-align: center;
+}
+
+.input-line {
+  border: 0;
+  outline: 0;
+  background: transparent;
+  border-bottom: 1px solid #FFD381;
+  font-size: 14px;
+  width: 40px;
+  height: 27px;
+  margin-top: 5px;
+  margin-left: 7px;
 }
 
 </style>
